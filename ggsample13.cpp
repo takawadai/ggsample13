82d653e46e9265d4fe3adfb66d2e7b7dc3aa0858
takas <s246017@wakayama-u.ac.jp>
2023-07-26 20:58:10 +0900

    鏡像をレンダリングする処理のところ，ライブラリの仕様が変わっており一筋縄ではいかないぞ

--- ggsample13.cpp
+++ ggsample13.cpp
@@ -213,15 +213,21 @@ int GgApp::main(int argc, const char* const* argv)
     // 画面消去
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    //鏡像をレンダリング
     mirror.use(light);
     light.loadPosition(reflect);
-
+   
     //前面にカリング
     glCullFace(GL_FRONT);
     drawObjects(mirror, mp * mr, object.get(), material, objects, t);
     glCullFace(GL_BACK);
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
     glViewport(0, 0, window.getWidth(), window.getHeight());
+
+    //正像をレンダリング
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    simple.use(light);
+    simple.
     /**
     // 正像用の光源の位置
     light.loadPosition(normal);
